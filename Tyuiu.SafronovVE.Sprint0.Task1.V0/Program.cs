226b103a22d916c81986349b834630f5ddb9090f
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-07 23:09:57 +0500

    Task1

---
+++ Tyuiu.SafronovVE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string name = "Владимир";
+        int age = 18;
+
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+        Console.ReadKey();
+    }
+}
