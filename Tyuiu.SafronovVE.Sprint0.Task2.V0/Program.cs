0a882b7e4eef2871398473888ac3d233680faddd
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-08 13:45:46 +0500

    Task2

---
+++ Tyuiu.SafronovVE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SafronovVE.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Владимир"));
+        Console.ReadKey();
+}
+}
